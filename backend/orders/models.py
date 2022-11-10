0e7da30aa71496529701a2223b42d0b9c369d3a9
wesleywil <wesleywilsonti@gmail.com>
2022-11-10 12:35:06 -0300

    updated model Order by adding status field

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -8,6 +8,13 @@ class Order(models.Model):
     orderItems = models.ManyToManyField("OrderItems", blank=True)
     total = models.DecimalField(max_digits=5, default=0.00, decimal_places=2)
 
+    class Status(models.IntegerChoices):
+        OPEN = 0
+        IN_PROGRESS = 1
+        CLOSED = 2
+    
+    status = models.IntegerField(Status.choices, default= Status.OPEN)
+
     def __str__(self):
         return f"{self.profile.user.username} - order total = $ {self.total}"
 
