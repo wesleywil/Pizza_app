5cb824f78036eedb7b1c1e5291dadb8a83c5e226
wesleywil <wesleywilsonti@gmail.com>
2022-11-10 13:38:09 -0300

    added library to handle nested models relation and updated order serializer

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+
 
 from .models import Order, OrderItems
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['id', 'profile', 'orderItems', 'total']
 
 class OrderItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItems
         fields = ['id', 'product', 'quantity', 'total']
+
+class OrderSerializer(WritableNestedModelSerializer):
+    orderItems = OrderItemsSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'profile', 'orderItems', 'total']
+
